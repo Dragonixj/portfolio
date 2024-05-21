d84e17e626a55d03a631e8a72db13981ba14e0c1
Dragonixj <delafiado@gmail.com>
2024-05-21 10:43:11 +0000

    tailwind setup

---
+++ src/shared/data/Projects.jsx
@@ -0,0 +1,11 @@
+const Projects = {
+    Project1: {
+        title: '....',
+        desc: '.....',
+        techStack: '.....',
+        open: 'side for deployments',
+        link: 'githubpage',
+        image: '../assets/....',
+    },
+};
+export default Projects;
