ff260e96300ab511fee1ad10679da2e7367d658d
Dragonixj <delafiado@gmail.com>
2024-05-29 15:52:30 +0000

    finished with a basic navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import MailIcon from '@mui/icons-material/Mail';
+import { FaGithub } from 'react-icons/fa';
 
 function NavBar() {
     return (
-        <nav>
-            <div></div>
+        <nav className="bg-gray-900 fixed top-0 w-full">
+            <div className="container mx-auto flex justify-between items-center py-2 px-6">
+                <div className="flex flex-grow justify-center space-x-4 text-white">
+                    <a href="#intro" className="font-bold">
+                        Intro
+                    </a>
+                    <a href="#about" className="font-bold">
+                        About
+                    </a>
+                    <a href="#projects" className="font-bold">
+                        Projects
+                    </a>
+                </div>
+                <div>
+                    <NavBarIcon icon={<FaGithub size="20" />} />
+                </div>
+            </div>
         </nav>
     );
 }
+
+const NavBarIcon = ({ icon }) => <div className="navbar-icon">{icon}</div>;
 
 export default NavBar;
