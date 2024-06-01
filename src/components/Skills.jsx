11597b201ceca20fd6968df1a683ef125c26b3f2
Dragonixj <delafiado@gmail.com>
2024-06-01 00:39:22 +0000

    skills section
    + imported all needed icons

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import { FaHtml5, FaCss3, FaReact } from 'react-icons/fa';
+import {
+    SiTailwindcss,
+    SiJavascript,
+    SiPython,
+    SiC,
+    SiFlask,
+    SiLinux,
+    SiNeovim,
+} from 'react-icons/si';
 
 function Skills() {
-    return <div>Skills</div>;
+    return (
+        <div>
+            {/* section header */}
+            <div>
+                <span></span>
+            </div>
+
+            <div></div>
+        </div>
+    );
 }
 
 export default Skills;
