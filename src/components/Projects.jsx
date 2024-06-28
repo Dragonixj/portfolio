707f7a4430867467dfc0f5f4d142f92e8c5c76fb
m0nztrum <delafiado@gmail.com>
2024-06-28 23:20:04 +0000

    perf: defined projectsCard outside project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,47 +2,48 @@ import React from 'react';
 import { projects } from '../constants';
 import { github } from '../assets';
 
-function Projects() {
-    const ProjectCard = ({ title, desc, techstack, open, link, image }) => {
-        return (
-            <div className="rounded-2xl bg-rosePine-overlay p-5 md:p-6">
-                <div className="relative w-full">
-                    <img
-                        src={image}
-                        alt={`${title}`}
-                        className="h-48 w-full rounded-xl object-cover"
-                    />
-                    <div className="absolute inset-0 m-3 flex justify-end">
-                        <div
-                            onClick={() => window.open(link, '_blank')}
-                            className="flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-black"
-                        >
-                            <img
-                                src={github}
-                                alt="gitrepo"
-                                className="object-contain"
-                            />
-                        </div>
+const ProjectCard = ({ title, desc, techstack, link, image }) => {
+    return (
+        <div className="rounded-2xl bg-rosePine-overlay p-5 md:p-6">
+            <div className="relative w-full">
+                <img
+                    src={image}
+                    alt={`${title}`}
+                    className="h-48 w-full rounded-xl object-cover"
+                />
+                <div className="absolute inset-0 m-3 flex justify-end">
+                    <div
+                        onClick={() => window.open(link, '_blank')}
+                        className="flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-black"
+                    >
+                        <img
+                            src={github}
+                            alt="gitrepo"
+                            className="object-contain"
+                        />
                     </div>
                 </div>
-
-                <div className="mt-5">
-                    <h3 className="text-lg font-bold text-rosePine-text sm:text-xl md:text-2xl lg:text-3xl">
-                        {title}
-                    </h3>
-                    <p className="mt-2 text-sm text-rosePine-foam">{desc}</p>
-                </div>
-
-                <div className="mt-4 flex flex-wrap gap-2">
-                    {techstack.map((tech, index) => (
-                        <p key={index} className={`text-[14px] ${tech.color}`}>
-                            #{tech.name}
-                        </p>
-                    ))}
-                </div>
             </div>
-        );
-    };
+
+            <div className="mt-5">
+                <h3 className="text-lg font-bold text-rosePine-text sm:text-xl md:text-2xl lg:text-3xl">
+                    {title}
+                </h3>
+                <p className="mt-2 text-sm text-rosePine-foam">{desc}</p>
+            </div>
+
+            <div className="mt-4 flex flex-wrap gap-2">
+                {techstack.map((tech, index) => (
+                    <p key={index} className={`text-[14px] ${tech.color}`}>
+                        #{tech.name}
+                    </p>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+function Projects() {
     return (
         <div
             id="projects"
