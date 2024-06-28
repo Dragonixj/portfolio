f4c4f153fa80d1c28215474c17ae742f52832e6d
m0nztrum <delafiado@gmail.com>
2024-06-28 23:14:22 +0000

    restructed assets folder

---
+++ src/assets/index.js
@@ -0,0 +1,23 @@
+import css from './techstack/css.png';
+import git from './techstack/git.png';
+import html from './techstack/html.png';
+import javascript from './techstack/javascript.png';
+import reactjs from './techstack/reactjs.png';
+import tailwind from './techstack/tailwind.png';
+
+import github from './github.png';
+
+import testimage from './proj/testimage.png';
+import shell from './proj/Shell.png';
+
+export {
+    css,
+    git,
+    html,
+    javascript,
+    reactjs,
+    tailwind,
+    testimage,
+    shell,
+    github,
+};
